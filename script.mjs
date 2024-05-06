327970df5ea67e02eb6b60fc233427905a7275a0
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-05-06 15:12:35 -0700

    Complete Part 1

---
+++ script.mjs
@@ -0,0 +1,42 @@
+// Part 1: Growing Pains
+// Your task is to create a program that advises a group of environmental scientists how to handle the growth and spread of a unique plant species under their supervision. You must develop a growth control system that will monitor and predict the plant growth, making decisions based on the available space and potential growth.
+// Here is the information you have been given:
+// The area in which the plants are contained is circular, with a radius of 5 meters.
+// The formula for calculating the area of a circle is PI multiplied by the radius, squared:
+// const PI = 3.1415;
+// const area = PI * radius * radius;
+// Each plant requires a minimum space of 0.8 square meters.
+// The area is starting with 20 plants.
+// The plants double in number every week.
+// Using this information, your objectives are to:
+// Predict the plant growth after a specific number of weeks.
+// Implement control flow to make decisions on whether the plants should be:
+// Pruned, to stop them from exceeding the capacity of the garden.
+// This condition should be met if the plant count after the given number of weeks is greater than 80% of the maximum capacity of the garden.
+// Monitored, if they are growing at an acceptable rate.
+// This condition should be met if the plant count is between 50% and 80% of the maximum capacity of the garden after the given number of weeks.
+// Planted, if there is room to plant more plants.
+// This condition should be met if the plant count after the given number of weeks is less than 50% of the maximum capacity of the garden.
+// Within your submission, include the results for 1, 2, and 3 weeks of growth as inputs.
+
+const radius = 5;
+const PI = 3.1415;
+const area = PI * radius * radius;
+
+// console.log(`Area: ${area}`);
+
+let numWeeks = 3;
+
+let plantAreaPerWeek = 0.8*((2**numWeeks)*10);
+let capacity = plantAreaPerWeek / area;
+
+console.log(`Week: ${numWeeks}`)
+console.log(`Plant Area Per Week: ${plantAreaPerWeek}`);
+console.log(`Capacity: ${capacity}`)
+
+if(capacity >= 0.8)
+    console.log(`Pruned`)
+else if(capacity >= .5)
+    console.log(`Monitored`)
+else
+    console.log(`Planted`)
